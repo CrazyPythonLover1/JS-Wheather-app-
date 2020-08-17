9674bebb04fc1259cc0a44bffd30032ba2788b30
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-17 13:51:09 +0600

    complete wheather API

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ window.addEventListener('load', ()=>{
     let temperatureDegree = document.querySelector(".temperature-degree");
     let temperatureDescription = document.querySelector('.temperature-description');
     let locationTimezone = document.querySelector('.location-timezone');
+    let conditionIcon = document.querySelector('.icon');
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -11,7 +12,7 @@ window.addEventListener('load', ()=>{
             lat = position.coords.latitude;
 
             const proxy = "http://cors-anywhere.herokuapp.com/";
-            const api = `http://api.weatherapi.com/v1/current.json?key=88ac78c11043448890d165322201308&q=${lat},${long}`;
+            const api = `http://api.weatherapi.com/v1/forecast.json?key=88ac78c11043448890d165322201308&q=${lat},${long}`;
 
             fetch(api)
             .then(response => {
@@ -20,18 +21,22 @@ window.addEventListener('load', ()=>{
 
             .then(data => {
                 console.log(data);
-                temperatureDegree.textContent = temp_f;
+                const { temp_f, temp_c,condition} = data.current;
+                temperatureDegree.textContent = `${temp_f} F`;
+                temperatureDegree.addEventListener("click", ()=>{
+                    temperatureDegree.textContent = `${temp_c} C`;
+                })
                 temperatureDescription.textContent = condition.text;
                 locationTimezone.textContent = data.location.tz_id;
                 // Set Icons
-                setIcons(icon, document.querySelector(".icon"));
+                conditionIcon.src = `http:${condition.icon}`;
             })
         });
     }
 
     function setIcons(icon, iconID){
-        const skycons = new Skycons({"color": "White"});
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        const skycons = new Skycons({"color": "white"});
+        const currentIcon = icon;
         skycons.play();
         return skycons.set(iconID, Skycons[currentIcon]);
     }
